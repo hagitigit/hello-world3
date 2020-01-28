0ae517e17e74f1601f93cbfdb2c400323021a7e7
hagitigit <60382230+hagitigit@users.noreply.github.com>
2020-01-28 11:49:51 +0200

    Add files via upload

---
+++ hello-world/Items_Detail_sample.js
@@ -0,0 +1,33 @@
+define(function () {
+	// Detail View
+	const VIEW_OITM_DETAIL = "fb229f12-b37f-4215-8550-df1889e5bf59";
+	
+	const UUID_BTN_HELLO_WORLD = "uuid_btn_hello_world";
+	
+
+	async function loadHelloWorld(oInst) {
+		oInst.showMessageBox("Success", "Hello World Button Clicked 😊", [{
+			label: "OK",
+			key: "OK"
+		}]);
+
+	}
+
+	async function myHelloWorldClicked(oInst) {
+
+	}
+
+	// Event names
+	const onAfterFormOpen = `OnAfterOpenForm`;
+	const onAfterButtonClickBtnHelloWorld = `on${UUID_BTN_HELLO_WORLD}AfterButtonClick`;
+
+	return {
+		[onAfterFormOpen]: async function (oInst) {
+			await loadHelloWorld(oInst);
+		},
+		[onAfterButtonClickBtnHelloWorld]: async function (oInst) {
+			await myHelloWorldClicked(oInst);
+		}
+		
+	};
+});
